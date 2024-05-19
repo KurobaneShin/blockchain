f3c368f7092890f1dab205c8eddfb9d70a027753
icaro <darkflamemaster663@gmail.com>
2024-05-19 17:08:41 -0300

    add missing transactions things

--- types/transaction.go
+++ types/transaction.go
@@ -1,1 +1,42 @@
 package types
+
+import (
+	"crypto/sha256"
+
+	pb "google.golang.org/protobuf/proto"
+
+	"github.com/KurobaneShin/blockchain/crypto"
+	"github.com/KurobaneShin/blockchain/proto"
+)
+
+func SignTransaction(pk *crypto.PrivateKey, tx *proto.Transaction) *crypto.Signature {
+	return pk.Sign(HashTransaction(tx))
+}
+
+func HashTransaction(tx *proto.Transaction) []byte {
+	b, err := pb.Marshal(tx)
+	if err != nil {
+		panic(err)
+	}
+
+	hash := sha256.Sum256(b)
+	return hash[:]
+}
+
+func VerifyTranscation(tx *proto.Transaction) bool {
+	for _, input := range tx.Inputs {
+		var (
+			sig    = crypto.SignatureFromBytes(input.Signature)
+			pubKey = crypto.PublicKeyFromBytes(input.PublicKey)
+		)
+
+		input.Signature = nil
+
+		if !sig.Verify(pubKey, HashTransaction(tx)) {
+			return false
+		}
+
+	}
+
+	return true
+}
--- types/transaction_test.go
+++ types/transaction_test.go
@@ -1,1 +1,46 @@
 package types
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/KurobaneShin/blockchain/crypto"
+	"github.com/KurobaneShin/blockchain/proto"
+	"github.com/KurobaneShin/blockchain/util"
+)
+
+func TestNewTransaction(t *testing.T) {
+	fromPrivKey := crypto.GeneratePrivateKey()
+	fromAddress := fromPrivKey.Public().Address().Bytes()
+
+	toPrivKey := crypto.GeneratePrivateKey()
+	toAddress := toPrivKey.Public().Address().Bytes()
+
+	input := &proto.TxInput{
+		PrevTxHash:   util.RandomHash(),
+		PrevOutIndex: 0,
+		PublicKey:    fromPrivKey.Public().Bytes(),
+	}
+
+	output1 := &proto.TxOutput{
+		Amount:  5,
+		Address: toAddress,
+	}
+
+	output2 := &proto.TxOutput{
+		Amount:  95,
+		Address: fromAddress,
+	}
+
+	tx := &proto.Transaction{
+		Version: 1,
+		Inputs:  []*proto.TxInput{input},
+		Outputs: []*proto.TxOutput{output1, output2},
+	}
+
+	sig := SignTransaction(fromPrivKey, tx)
+	input.Signature = sig.Bytes()
+
+	assert.True(t, VerifyTranscation(tx))
+}
